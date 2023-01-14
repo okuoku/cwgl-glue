b0122cd7d89c68720451d55c45e28afbd5f1542a
okuoku <mjt@cltn.org>
2023-01-14 21:23:00 +0900

    Add function list

---
+++ src/glue-functbl.c
@@ -0,0 +1,26 @@
+#include "glue-priv.h"
+#include "glue-funclist.h"
+#include <string.h>
+
+CWGL_GLUE_EXPORT_EGL EGLDisplay KHRONOS_APIENTRY eglGetDisplay(EGLNativeDisplayType display_id);
+CWGL_GLUE_EXPORT_EGL EGLBoolean KHRONOS_APIENTRY eglInitialize(EGLDisplay dpy, EGLint *major, EGLint *minor);
+CWGL_GLUE_EXPORT_EGL EGLBoolean KHRONOS_APIENTRY eglChooseConfig(EGLDisplay dpy, const EGLint *attrib_list, EGLConfig *configs, EGLint config_size, EGLint *num_config);
+CWGL_GLUE_EXPORT_EGL EGLSurface KHRONOS_APIENTRY eglCreateWindowSurface(EGLDisplay dpy, EGLConfig config, EGLNativeWindowType win, const EGLint *attrib_list);
+CWGL_GLUE_EXPORT_EGL EGLBoolean KHRONOS_APIENTRY eglBindAPI(EGLenum api);
+CWGL_GLUE_EXPORT_EGL EGLContext  KHRONOS_APIENTRY eglCreateContext(EGLDisplay dpy, EGLConfig config, EGLContext share_context, const EGLint *attrib_list);
+CWGL_GLUE_EXPORT_EGL EGLBoolean KHRONOS_APIENTRY eglDestroyContext(EGLDisplay dpy, EGLContext ctx);
+CWGL_GLUE_EXPORT_EGL EGLBoolean KHRONOS_APIENTRY eglMakeCurrent(EGLDisplay dpy, EGLSurface draw, EGLSurface read, EGLContext ctx);
+CWGL_GLUE_EXPORT_EGL EGLBoolean KHRONOS_APIENTRY eglSwapBuffers(EGLDisplay dpy, EGLSurface surface);
+CWGL_GLUE_EXPORT_EGL void* KHRONOS_APIENTRY eglGetProcAddress(const char *procname);
+
+void*
+cwgl_glue_lookup(const char* name){
+#define ITR_CHECK(func) \
+    if(!strcmp(name, #func)){ \
+        return func; \
+    }
+FUNC_LIST(ITR_CHECK);
+    return 0;
+}
+
+
--- src/glue-priv.h
+++ src/glue-priv.h
@@ -1,9 +1,16 @@
 #include <stdint.h>
 //FIXME: Merge it with egl.h ??
+typedef void* EGLDisplay;
 typedef void* EGLConfig;
 typedef void* EGLSurface;
 typedef void* EGLContext;
+typedef unsigned int EGLBoolean;
 typedef int32_t EGLint;
+typedef unsigned int EGLenum;
+
+// Yuniframe specific
+typedef uintptr_t EGLNativeDisplayType;
+typedef uintptr_t EGLNativeWindowType;
 
 #define CWGL_GLUE_EXPORT_EGL __declspec(dllexport)
 
