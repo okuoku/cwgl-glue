f7532d6cb0028961ac3b9a38b24617aacd044bcd
okuoku <mjt@cltn.org>
2023-01-15 06:36:48 +0900

    Wrap GLES (s5)

--- src/glue-s5.c
+++ src/glue-s5.c
@@ -1,17 +1,30 @@
 #include "glue-priv.h"
+#include "glue-ctx.h"
 
 /* 5.1 Flush and Finish */
 GL_APICALL void GL_APIENTRY
 glFlush(void){
+    cwgl_ctx_t* ctx;
+    ctx = glue_current_ctx();
+
+    cwgl_flush(ctx);
 }
 
 GL_APICALL void GL_APIENTRY
 glFinish(void){
+    cwgl_ctx_t* ctx;
+    ctx = glue_current_ctx();
+
+    cwgl_finish(ctx);
 }
 
 /* 5.2 Hints */
 GL_APICALL void GL_APIENTRY
 glHint(GLenum target, GLenum hint){
+    cwgl_ctx_t* ctx;
+    ctx = glue_current_ctx();
+
+    cwgl_hint(ctx, (cwgl_enum_t) target, (cwgl_enum_t) hint);
 }
 
 
