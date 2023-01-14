c8f06122bbfedd8189c87fe87814828efa243f55
okuoku <mjt@cltn.org>
2023-01-15 06:13:37 +0900

    Wrap GLES (snone)

--- src/glue-snone.c
+++ src/glue-snone.c
@@ -4,8 +4,10 @@
 
 GL_APICALL void GL_APIENTRY
 glEnable(GLenum cap){
+    cwgl_enable(glue_current_ctx(), (cwgl_enum_t) cap);
 }
 
 GL_APICALL void GL_APIENTRY
 glDisable(GLenum cap){
+    cwgl_disable(glue_current_ctx(), (cwgl_enum_t) cap);
 }
