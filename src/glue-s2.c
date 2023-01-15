a635eba6a2d761b56d8e4ef37b75eca103ca4241
okuoku <mjt@cltn.org>
2023-01-15 19:27:15 +0900

    Wrap GLES (s2, obj)

--- src/glue-s2.c
+++ src/glue-s2.c
@@ -1,5 +1,10 @@
 #include "glue-priv.h"
 #include "glue-ctx.h"
+#include <stdlib.h>
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <string.h>
 
 /* 2.5 GL Errors */
 GL_APICALL GLenum GL_APIENTRY
@@ -93,94 +98,332 @@ glDrawElements(GLenum mode, GLsizei count, GLenum type,
 /* 2.9 Buffer Objects */
 GL_APICALL void GL_APIENTRY
 glBindBuffer(GLenum target, GLuint buffer){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    cwgl_Buffer_t* buf;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    if(! buffer){
+        buf = 0;
+    }else{
+        ptr = glue_get(glue, OBJ_BUFFER, buffer);
+        if(! ptr){
+            abort(); // unknown
+            return;
+        }else{
+            buf = ptr->buffer;
+        }
+    }
+    cwgl_bindBuffer(ctx, (cwgl_enum_t) target, buf);
+    // FIXME: Track binding here
 }
 
 GL_APICALL void GL_APIENTRY
 glDeleteBuffers(GLsizei n, const GLuint *buffers){
-    // FIXME:
+    GLsizei x;
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    for(x = 0;x != n; x++){
+        if(buffers[x]){
+            ptr = glue_get(glue, OBJ_BUFFER, buffers[x]);
+            if(! ptr){
+                abort(); // unknown
+                return;
+            }
+            cwgl_deleteBuffer(ctx, ptr->buffer);
+            (void) glue_del(glue, OBJ_BUFFER, buffers[x]);
+            // FIXME: Track binding here
+        }
+    }
+
 }
 
 GL_APICALL void GL_APIENTRY
 glGenBuffers(GLsizei n, GLuint *buffers){
-    // FIXME:
+    GLsizei x;
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t ptr;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    for(x = 0; x != n; x++){
+        ptr.buffer = cwgl_createBuffer(ctx);
+        buffers[x] = glue_put(glue, OBJ_BUFFER, &ptr);
+    }
 }
 
 GL_APICALL void GL_APIENTRY
 glBufferData(GLenum target, GLsizeiptr size, const void *data, GLenum usage){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    ctx = glue_current_ctx();
+
+    cwgl_bufferData(ctx, (cwgl_enum_t) target, size, data, (cwgl_enum_t) usage);
 }
 
 GL_APICALL void GL_APIENTRY
 glBufferSubData(GLenum target, GLintptr offset, GLsizeiptr size,
                 const void *data){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    ctx = glue_current_ctx();
+
+    cwgl_bufferSubData(ctx, (cwgl_enum_t) target, offset, data, size);
 }
 
 /* 2.10 Vertex Shaders */
 /* 2.10.1 Loading and Compiling Shader Source */
 GL_APICALL GLuint GL_APIENTRY
 glCreateShader(GLenum type){
-    // FIXME:
+    GLuint r;
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t ptr;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    ptr.shader = cwgl_createShader(ctx, (cwgl_enum_t) type);
+    r = glue_put(glue, OBJ_SHADER, &ptr);
+
+    return r;
 }
 
 GL_APICALL void GL_APIENTRY
 glShaderSource(GLuint shader, GLsizei count, const GLchar *const*string,
                const GLint *length){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    cwgl_Shader_t* sh;
+    size_t x,y,srctotal;
+    char* temp;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    if(! shader){
+        abort(); // error
+    }else{
+        ptr = glue_get(glue, OBJ_SHADER, shader);
+        if(! ptr){
+            abort(); // unknown
+            return;
+        }
+        sh = ptr->shader;
+    }
+
+    /* pass1: Get total length */
+    srctotal = 0;
+    if(! length){
+        x = 0;
+        while(string[x]){
+            srctotal += strlen(string[x]);
+            x++;
+        }
+    }else{
+        x = 0;
+        while(string[x]){
+            srctotal += length[x];
+            x++;
+        }
+    }
+    temp = malloc(srctotal + 1);
+    if(! temp){
+        abort();
+    }
+
+    /* pass2: Copy to temporary buffer */
+    if(! length){
+        x = 0;
+        temp[0] = 0;
+        while(string[x]){
+            (void) strcat(temp, string[x]);
+            x++;
+        }
+    }else{
+        x = 0;
+        y = 0;
+        while(string[x]){
+            memcpy(&temp[y], string[x], length[x]);
+            y += length[x];
+            x++;
+        }
+        temp[y] = 0;
+    }
+
+    cwgl_shaderSource(ctx, sh, temp, srctotal);
+    free(temp);
+
 }
 
 GL_APICALL void GL_APIENTRY
 glCompileShader(GLuint shader){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    cwgl_Shader_t* sh;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    if(! shader){
+        abort(); // error
+    }else{
+        ptr = glue_get(glue, OBJ_SHADER, shader);
+        if(! ptr){
+            abort(); // unknown
+            return;
+        }
+        sh = ptr->shader;
+    }
+
+    cwgl_compileShader(ctx, sh);
 }
 
 GL_APICALL void GL_APIENTRY
 glReleaseShaderCompiler(void){
-    // FIXME:
+    /* Do nothing */
 }
 
 GL_APICALL void GL_APIENTRY
 glDeleteShader(GLuint shader){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+    ptr = glue_get(glue, OBJ_SHADER, shader);
+    if(! ptr){
+        abort(); // error
+    }
+    cwgl_deleteShader(ctx, ptr->shader);
+    (void) glue_del(glue, OBJ_SHADER, shader);
 }
 
 /* 2.10.2 Loading Shader Binaries */
 GL_APICALL void GL_APIENTRY
 glShaderBinary(GLsizei count, const GLuint *shaders,
                GLenum binaryformat, const void *binary, GLsizei length){
-    // FIXME:
+    abort();
 }
 
 /* 2.10.3 Program Objects */
 GL_APICALL GLuint GL_APIENTRY
 glCreateProgram(void){
-    // FIXME:
+    GLuint r;
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t ptr;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    ptr.program = cwgl_createProgram(ctx);
+    r = glue_put(glue, OBJ_PROGRAM, &ptr);
+
+    return r;
 }
 
 GL_APICALL void GL_APIENTRY
 glAttachShader(GLuint program, GLuint shader){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    cwgl_Shader_t* sh;
+    cwgl_Program_t* prg;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    if(shader){
+        ptr = glue_get(glue, OBJ_SHADER, shader);
+        sh = ptr->shader;
+    }else{
+        sh = 0;
+    }
+    ptr = glue_get(glue, OBJ_PROGRAM, program);
+    if(! ptr){
+        abort(); // error
+    }
+    prg = ptr->program;
+
+    cwgl_attachShader(ctx, prg, sh);
 }
 
 GL_APICALL void GL_APIENTRY
 glDetachShader(GLuint program, GLuint shader){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    cwgl_Shader_t* sh;
+    cwgl_Program_t* prg;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    ptr = glue_get(glue, OBJ_SHADER, shader);
+    if(! ptr){
+        abort(); // error
+    }
+    sh = ptr->shader;
+    ptr = glue_get(glue, OBJ_PROGRAM, program);
+    if(! ptr){
+        abort(); // error
+    }
+    prg = ptr->program;
+
+    cwgl_attachShader(ctx, prg, sh);
 }
 
 GL_APICALL void GL_APIENTRY
 glLinkProgram(GLuint program){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    cwgl_Program_t* prg;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    ptr = glue_get(glue, OBJ_PROGRAM, program);
+    if(! ptr){
+        abort(); // error
+    }
+    prg = ptr->program;
+
+    cwgl_linkProgram(ctx, prg);
 }
 
 GL_APICALL void GL_APIENTRY
 glUseProgram(GLuint program){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    cwgl_Program_t* prg;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    ptr = glue_get(glue, OBJ_PROGRAM, program);
+    if(! ptr){
+        abort(); // error
+    }
+    prg = ptr->program;
+
+    cwgl_useProgram(ctx, prg);
 }
 
 GL_APICALL void GL_APIENTRY
 glDeleteProgram(GLuint program){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+    ptr = glue_get(glue, OBJ_PROGRAM, program);
+    if(! ptr){
+        abort(); // error
+    }
+    cwgl_deleteProgram(ctx, ptr->program);
+    (void) glue_del(glue, OBJ_PROGRAM, program);
 }
 
 /* 2.10.4 Shader Variables */
@@ -314,7 +557,20 @@ glUniformMatrix4fv(GLint location, GLsizei count, GLboolean transpose,
 /* 2.10.5 Shader Execution */
 GL_APICALL void GL_APIENTRY
 glValidateProgram(GLuint program){
-    // FIXME:
+    cwgl_ctx_t* ctx;
+    glue_ctx_t* glue;
+    glue_obj_ptr_t* ptr;
+    cwgl_Program_t* prg;
+    ctx = glue_current_ctx();
+    glue = glue_current_glue();
+
+    ptr = glue_get(glue, OBJ_PROGRAM, program);
+    if(! ptr){
+        abort(); // error
+    }
+    prg = ptr->program;
+
+    cwgl_validateProgram(ctx, prg);
 }
 
 /* 2.12 Coordinate Transformations */
